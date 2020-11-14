795f59d8624860183178fca453a4a430d123b9be
Charlie-Eeles <41295345+Charlie-Eeles@users.noreply.github.com>
2020-11-14 19:52:27 +0000

    Unit conversion commit
    in this commit:
    -Changed filename Rosto to Wise to reflext functionality shift
    -Added command for unit conversion !cv

--- Wise.py
+++ Wise.py
@@ -4,20 +4,24 @@ import requests
 import sys
 import webbrowser
 import bs4
+from wit import Wit
 from discord.ext import commands
 from dotenv import load_dotenv
+from unit_converter.converter import convert, converts
 
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
+# WIT_TOKEN = os.getenv("WIT_TOKEN")
+# witclient = Wit(WIT_TOKEN)
 translator = googletrans.Translator()
 
 bot = commands.Bot(command_prefix='!')
 
-@bot.command(name="jc", help="Alexandria joins voice channel.")
+@bot.command(name="jc", help="Wise joins voice channel.")
 async def join(ctx):
     channel = ctx.author.voice.channel
     await channel.connect()
-@bot.command(name="lc", help="Alexandria leaves voice channel.")
+@bot.command(name="lc", help="Wise leaves voice channel.")
 async def leave(ctx):
     await ctx.voice_client.disconnect()
 
@@ -34,6 +38,12 @@ async def parse(ctx, x):
     final_text = text.split(". ",3)[:3]
     await ctx.send(f"{final_text[0]}. {final_text[1]}. {final_text[2]}.")
 
+@bot.command(name="cv", help="Converts units from x to y")
+async def convert_func(ctx, x, y):
+    ans = float(converts(x, y))
+    await ctx.send(f"{ans:.2f}")
+    
+
 @translate_func.error
 async def on_translation_error(ctx, error):
     await ctx.send("An error has been raised, please follow the syntax of '!tr target-language text-to-translate', if that still doesn't work then the language you're trying to translate from or translate to might not be supported.")
@@ -42,11 +52,13 @@ async def on_translation_error(ctx, error):
 async def on_parse_error(ctx, error):
     await ctx.send("I'm sorry, I couldn't find that article. please remember that I only search anglophone wikipedia and if your article is more than one word it must be wrapped in quotation marks.") 
 
-
 @join.error
 async def on_join_error(ctx, error):
     await ctx.send("I'm sorry I can't join the voice channel you're in at the moment, please double check permissions and try again.")
 
+@convert_func.error
+async def on_convert_error(ctx, error):
+    await ctx.send("Either the units you're using are unsupported or incompatible, please try again with new units.")
 
 bot.run(TOKEN)
 
@@ -60,6 +72,7 @@ bot.run(TOKEN)
     #pip install beautifulsoup4
     #pip install wit
     #pip install pynacl
+    #pip install unit-converter
     #A separate file in the same directory named ".env" 
         #containing a variable "DISCORD_TOKEN = 'your-discord-bot-token'"
 
