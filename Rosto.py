d11536735facb89c46ed25f1c7e5e381a0a37e56
Charlie-Eeles <41295345+Charlie-Eeles@users.noreply.github.com>
2020-11-14 02:17:48 +0000

    First functional commit
    The bot at this point has this functionality:
    - translates sentecnes in discord with the !tr {language} {text} command
    - catches error message when the wrong format is used
    - stores discord key in a separate dotenv file
    
    The api being used is however unstable, the fix if you get a nontype error message is to kill the terminal and restart, once it works once it'll keep working until the terminal is killed again.

--- Rosto.py
+++ Rosto.py
@@ -1,14 +1,22 @@
 import os
-import discord
+import googletrans
+from discord.ext import commands
 from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
+translator = googletrans.Translator()
 
-client = discord.Client()
+bot = commands.Bot(command_prefix='!')
 
-@client.event
-async def on_ready():
-    print(f"{client.user} has connected to Discord!")
+@bot.command(name='tr', help='Translates from auto detected language to a specified language.')
+async def translate_func(ctx, i, x):
+    tr = translator.translate(text= x, dest=i, src="auto")
+    await ctx.send(f"'{tr.text}' translated to {i} from {tr.src}")
 
-client.run(TOKEN)
+@translate_func.error
+async def on_command_error(ctx, error):
+    await ctx.send("An error has been raised, please follow the syntax of '!tr target-language text-to-translate', if that still doesn't work then the language you're trying to translate from or to might not be supported.")
+
+bot.run(TOKEN)
+
